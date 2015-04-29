7ab734b8a516d60ca3506c7f89e474854bbc7e01
Andrew Quitadamo <andrew.quitadamo@gmail.com>
2015-04-29 06:57:01 -0400

    Fixed missing cvrt and added qqplot option
    cvrt option was missing in the Matrix_eQTL_main function call.
    Added an option to output the qqplot to a pdf file.

--- R/mxeqtl.R
+++ R/mxeqtl.R
@@ -2,10 +2,12 @@
 mxeqtl <-
 function(snp_file,snp_location,expr_file,expr_location,cis_output_file,
          cis_pval,covariates="",trans_output_file="", trans_pval=0, 
-         model="linear", MAF=0, cis_dist=1e6, missing="NA")
+         model="linear", MAF=0, cis_dist=1e6, qq="")
 {
 # Matrix eQTL function based on the sample code by Andrey A. Shabalin
 # http://www.bios.unc.edu/research/genomic_software/Matrix_eQTL/
+
+base.dir<-"./";
 
 # Linear model to use, modelANOVA, modelLINEAR, or modelLINEAR_CROSS
 # modelANOVA, modelLINEAR, or modelLINEAR_CROSS
@@ -22,23 +24,20 @@ else if (model=="linear_cross")
 	useModel = modelLINEAR_CROSS
 }
 
-missing_data=missing;
-
 # Genotype file name
-SNP_file_name = snp_file;
-snps_location_file_name = snp_location;
+SNP_file_name = paste(base.dir,snp_file,sep="");
+snps_location_file_name = paste(base.dir,snp_location,sep="");
 
 # Gene expression file name
-expression_file_name = expr_file;
-gene_location_file_name = expr_location;
-
+expression_file_name = paste(base.dir,expr_file,sep="");
+gene_location_file_name = paste(base.dir,expr_location,sep="");
 
 # Covariates file name
 # Set to character() for no covariates
 if (covariates!="")
 {
-	#covariates_file_name = paste(base.dir,covaraites,sep="");
-  covariates_file_name = covariates;
+	covariates_file_name = paste(base.dir,covariates,sep="");
+	cat(covariates_file_name,'\n');
 }
 else
 {
@@ -46,10 +45,10 @@ else
 }
 # Output file name
 output_file_name = tempfile();
-output_file_name_cis = cis_output_file;
+output_file_name_cis = paste(base.dir,cis_output_file,sep="")
 if (trans_output_file!="")
 {
-  output_file_name_tra = trans_output_file;
+	output_file_name_tra = paste(base.dir,trans_output_file,sep="")
 }
 else
 {
@@ -72,7 +71,7 @@ cisDist = cis_dist;
 ## Load genotype data
 snps = SlicedData$new();
 snps$fileDelimiter = "\t";      # the TAB character
-snps$fileOmitCharacters = missing; # denote missing values;
+snps$fileOmitCharacters = "NA"; # denote missing values;
 snps$fileSkipRows = 1;          # one row of column labels
 snps$fileSkipColumns = 1;       # one column of row labels
 snps$fileSliceSize = 2000;      # read file in slices of 2,000 rows
@@ -97,10 +96,12 @@ if (MAF>0)
 	cat('SNPs after filtering:',nrow(snps),'\n')
 }
 
+snps$SaveFile("./meQTL_filtered_input")
+
 ## Load gene expression data
 gene = SlicedData$new();
 gene$fileDelimiter = "\t";      # the TAB character
-gene$fileOmitCharacters = missing; # denote missing values;
+gene$fileOmitCharacters = "NA"; # denote missing values;
 gene$fileSkipRows = 1;          # one row of column labels
 gene$fileSkipColumns = 1;       # one column of row labels
 gene$fileSliceSize = 2000;      # read file in slices of 2,000 rows
@@ -109,7 +110,7 @@ gene$LoadFile(expression_file_name);
 ## Load covariates
 cvrt = SlicedData$new();
 cvrt$fileDelimiter = "\t";      # the TAB character
-cvrt$fileOmitCharacters = missing; # denote missing values;
+cvrt$fileOmitCharacters = "NA"; # denote missing values;
 cvrt$fileSkipRows = 1;          # one row of column labels
 cvrt$fileSkipColumns = 1;       # one column of row labels
 if(length(covariates_file_name)>0) {
@@ -124,9 +125,12 @@ genepos = read.table(gene_location_file_name, header = TRUE, stringsAsFactors = 
 ## Set pvOutputThreshold = 0 and pvOutputThreshold.cis > 0 to perform eQTL analysis for local gene-SNP pairs only. 
 ## Local associations significant at pvOutputThreshold.cis level will be recorded in output_file_name.cis.
 
+
+#min.pv.by.genesnp set to TRUE for permutation analysis
 me = Matrix_eQTL_main(
   snps = snps,
   gene = gene,
+  cvrt = cvrt,
   output_file_name     = output_file_name_tra,
   pvOutputThreshold     = pvOutputThreshold_tra,
   useModel = useModel,
@@ -138,7 +142,7 @@ me = Matrix_eQTL_main(
   genepos = genepos,
   cisDist = cisDist,
   pvalue.hist = "qqplot",
-  min.pv.by.genesnp = FALSE,
+  min.pv.by.genesnp = TRUE,
   noFDRsaveMemory = FALSE);
 
 #unlink(output_file_name_tra);
@@ -152,7 +156,16 @@ cat('Detected ',me$trans$neqtls,' distant eQTLs:', '\n');
 #show(me$trans$eqtls)
 
 ## Plot the Q-Q plot of local and distant p-values
-plot(me)
+if (qq!="")
+{
+	pdf(qq)
+	plot(me)
+	dev.off()
+}
+else
+{
+	plot(me)
+}
 
 ## Auxiliar plot + change legends
 #plot(GD462_CEU_OldGen,xmin=1e-09,ymin=1e-165,main=NULL)
