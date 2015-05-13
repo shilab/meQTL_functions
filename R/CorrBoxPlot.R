2bb7645518e43d866f678a4bfe00f5b31c37bca4
Andrew Quitadamo <aquitada@uncc.edu>
2015-05-13 08:51:21 -0400

    Reformatted curly braces

--- R/CorrBoxPlot.R
+++ R/CorrBoxPlot.R
@@ -1,5 +1,6 @@
 #' @export
-CorrBoxPlot <- function (mEQTL,threshold,expr,genot,visual=FALSE,pdf_file="",crlt=0,cis=TRUE) {
+CorrBoxPlot <- function (mEQTL,threshold,expr,genot,visual=FALSE,pdf_file="",crlt=0,cis=TRUE)
+{
 	# Inputs:
 	#   mEQTL     - Matrix EQTL object with the eQTLs already collected
 	#   threshold - FDR cutoff, only those eQTLs with equal or lower threshold will be taken into account
@@ -24,44 +25,53 @@ CorrBoxPlot <- function (mEQTL,threshold,expr,genot,visual=FALSE,pdf_file="",crl
 
 	corr  <- NULL; phenotype <- NULL; genotype <- NULL; phenotype_new<-NULL;
 
-	if (cis==TRUE) {
+	if (cis==TRUE)
+	{
 		index <- which(mEQTL$cis$eqtls$FDR<=threshold)
 		eqtls <- mEQTL$cis$eqtls[index,]
 	}
-	else {
+	else
+	{
 		index <- which(mEQTL$trans$eqtls$FDR<=threshold)
 		eqtls <- mEQTL$trans$eqtls[index,]    
 	}
 
-	for (i in 1:nrow(eqtls)) {
+	for (i in 1:nrow(eqtls))
+	{
 		phenotype[[i]] <- expr[which(expr[,1]==as.character(eqtls$gene[i])),2:ncol(expr)]
 		genotype[[i]]  <- genot[which(genot[,1]==as.character(eqtls$snps[i])),2:ncol(genot)]
 		corr[i]   <- cor(as.numeric(phenotype[[i]]),as.numeric(genotype[[i]]), use="pairwise.complete.obs")
 	}
 
-	if (visual) {
-		if (pdf_file!="") {
+	if (visual)
+	{
+		if (pdf_file!="")
+		{
 			pdf(paste('./',pdf_file,sep=""))
 			par(mfcol = c(2, 2))
 		}
 		genotypes <- range(genot[,2:ncol(genot)])[1]:range(genot[,2:ncol(genot)])[2]
-		for (i in 1:nrow(eqtls)) {
+		for (i in 1:nrow(eqtls))
+		{
 			#Prepare the matrix
 			pheno <- as.numeric(phenotype[[i]])
 			values <- list();
-			for (j in 1:length(genotypes)) {
+			for (j in 1:length(genotypes))
+			{
 				values[[j]] <- pheno[which(genotype[[i]]==genotypes[j])]
 			}
 			#Plot the boxplots
 			cats=seq(0,length(genotypes)-1)
-			if (abs(corr[i])>=crlt) {
+			if (abs(corr[i])>=crlt)
+			{
 				boxplot(values,boxwex=0.5,ylab=paste(as.character(eqtls$gene[i])," expression"), names=cats,
 					xlab=paste(as.character(eqtls$snps[i])," genotype","\nCorrelation: ",format(corr[i],2),
 					"P-value: ",format(eqtls$pvalue[i],2)," FDR: ",format(eqtls$FDR[i],2)),
 		  			main=paste(as.character(eqtls$snps[i])," - ",as.character(eqtls$gene[i])))
 			}
 		}
-		if (pdf_file!="") {
+		if (pdf_file!="")
+		{
 			dev.off()
 		}
 	}
