d689e5eba2f53e0142b441a99e04f4ac3ce328d5
Andrew Quitadamo <andrew.quitadamo@gmail.com>
2015-05-30 11:06:33 -0400

    Added tests for setSNPOptions

--- tests/testthat/test-meQTLfunc.R
+++ tests/testthat/test-meQTLfunc.R
@@ -1,6 +1,15 @@
-test_that("Testing setModel function", {
+test_that("setModel function works", {
 	expect_error(setModel(''))
 	expect_equal(setModel('linear'),modelLINEAR)
 	expect_equal(setModel('anova'),modelANOVA)
 	expect_equal(setModel('linear_cross'),modelLINEAR_CROSS)
+})
+
+test_that("setSNPOptions function works", {
+	snps<-setSNPOptions("\t", "NA")
+	expect_equal(snps$fileDelimiter, "\t")
+	expect_equal(snps$fileOmitCharacters, "NA")
+	snps<-setSNPOptions(" ", "NaN")
+    expect_equal(snps$fileDelimiter, " ")
+    expect_equal(snps$fileOmitCharacters, "NaN")
 })
