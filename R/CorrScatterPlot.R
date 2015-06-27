bcab0a188b877678694ec6742e16b46438c7e900
Andrew Quitadamo <andrew.quitadamo@gmail.com>
2015-06-27 07:13:58 -0400

    Fixed functionality

--- R/CorrScatterPlot.R
+++ R/CorrScatterPlot.R
@@ -45,7 +45,6 @@ CorrScatterPlot <- function (mEQTL,threshold,expr,genot,visual=TRUE,cis=TRUE)
   if (visual)
   { #Perform the plots
     #There can be three values (unphased) or four (phased)
-    genotypes <- range(genot[,2:ncol(genot)])[1]:range(genot[,2:ncol(genot)])[2]
     for (i in 1:nrow(eqtls))
     {
       #Prepare the matrix
@@ -53,11 +52,6 @@ CorrScatterPlot <- function (mEQTL,threshold,expr,genot,visual=TRUE,cis=TRUE)
       pheno <- as.numeric(phenotype[[i]])
       #      geno
       #      pdf("scatterplot.pdf")
-      values <- NULL;
-      for (j in 1:length(genotypes))
-      {
-        values[[j]] <- pheno[which(genotype[[i]]==genotypes[j])]
-      }
       #      #Plot the boxplots
       #      if (length(genotypes)==3){cats=c(0,1,2)}
       #      else {cats=c(0,1,2,3)}
@@ -65,7 +59,7 @@ CorrScatterPlot <- function (mEQTL,threshold,expr,genot,visual=TRUE,cis=TRUE)
       #              xlab=paste(as.character(eqtls$snps[i])," genotype","\nCorrelation: ",format(corr[i],2),
       #                         "P-value: ",format(eqtls$pvalue[i],2)," FDR: ",format(eqtls$FDR[i],2)),
       #              main=paste(as.character(eqtls$snps[i])," - ",as.character(eqtls$gene[i])))
-      plot(values, xlab="miRNA Expression", ylab="mRNA Expression", main = paste(as.character(eqtls$snps[i])," genotype","\nCorrelation: ",format(corr[i],2),"P-value: ",format(eqtls$pvalue[i],2)," FDR: ",format(eqtls$FDR[i],2)))
+      plot(pheno, geno, xlab="miRNA Expression", ylab="mRNA Expression", main = paste(as.character(eqtls$snps[i])," genotype","\nCorrelation: ",format(corr[i],2),"P-value: ",format(eqtls$pvalue[i],2)," FDR: ",format(eqtls$FDR[i],2)))
       #	      abline(lm(geno ~ pheno)) 
       
     }
