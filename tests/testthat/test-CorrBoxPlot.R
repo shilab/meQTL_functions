c3f5e804d3e313ef21dc1b22ab4f63da0a94436d
Andrew Quitadamo <aquitada@uncc.edu>
2015-12-02 15:08:49 -0500

    Fixed me object creation

--- tests/testthat/test-CorrBoxPlot.R
+++ tests/testthat/test-CorrBoxPlot.R
@@ -1,18 +1,14 @@
 test_that("getIndex works", {
-	me$cis$eqtls<-list()
-	me$cis$eqtls$FDR<-c(0.1,0.05,0.01,0.2)
+	me<-list(cis=list(eqtls=list(FDR=c(0.1,0.05,0.01,0.2))))
 	expect_equal(getIndex(TRUE,me,0.1),c(1,2,3))
 	#TODO: Will need to fix
 	expect_equal(getIndex(TRUE,me,0),integer(0))
-	me$trans$eqtls<-list()
-	me$trans$eqtls$FDR<-c(0.1,0.05,0.01,0.2)
+	me<-list(trans=list(eqtls=list(FDR=c(0.1,0.05,0.01,0.2))))
 	expect_equal(getIndex(FALSE,me,0.1),c(1,2,3))
 })
 
 test_that("getEQTLS works", {
-	me<-list()
-	me$cis<-list()
-	me$cis$eqtls<-data.frame(snps=c('snp1','snp2'),gene=c('gene2','gene2'),statistic=c(6.09,5.97),pvalue=c(0.04e-01,4.23e-1),FDR=c(0.02,0.012),beta=c(3.78,4.49))
+	me<-list(cis=list(eqtls<-data.frame(snps=c('snp1','snp2'),gene=c('gene2','gene2'),statistic=c(6.09,5.97),pvalue=c(0.04e-01,4.23e-1),FDR=c(0.02,0.012),beta=c(3.78,4.49)) ))
 
 	index <- c(1)
 
@@ -29,4 +25,9 @@ test_that("getEQTLS works", {
 	res <- me$trans$eqtls[2,]
 
 	expect_equal(getEQTLS(FALSE,me,index),res)
+})
+
+test_that("getCorr works", {
+	phenotype <- c(1,1,1,2,2,2)
+	genotype <- c(1.5,1,1.5,2.5,2,2.5)	
 })
